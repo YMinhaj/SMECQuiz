e78687ff470bc05cad3dc5f097cc388ffc32c2da
Yousuf Minhaj <yousufminhajym@gmail.com>
2017-03-04 12:36:07 +0500

    first commit

---
+++ application/config/sq_config.php
@@ -0,0 +1,7 @@
+<?php 
+		$sq_base_url='http://192.168.0.100/';
+		$sq_hostname='localhost';
+		$sq_dbname='user';
+		$sq_dbusername='root';
+		$sq_dbpassword='';
+		?>
---
+++ application/views/myaccount.php
@@ -0,0 +1,27 @@
+ <div class="container">
+
+   
+ <h3><?php echo $title;?></h3>
+   
+ 
+
+  <div class="row">
+     <form method="post" action="<?php echo site_url('user/update_user/'.$uid);?>">
+	
+<div class="col-md-8">
+<br> 
+ 
+ 
+ <img src="../../../images/uc.gif"/>
+ 
+ 
+
+</div>
+
+
+
+ 
+
+
+
+</div>
